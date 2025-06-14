4d243511d0604dc50f832df579ddc633942bbb24
Bruno Henrique da Costa <bruno.costa@mobiis.com.br>
2025-06-14 17:11:53 -0300

    Add LinkedIn redirect route to web.php

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => '');
 Route::get('/github', fn () => redirect('https://github.com/bhcosta90'));
+Route::get('/linkedin', fn () => redirect('https://www.linkedin.com/in/bhcosta90'));
