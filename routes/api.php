8bb0fa6c80b48072ea038bc038e4193a6bbf6987
Bruno Costa <bhcosta90@gmail.com>
2025-06-24 00:13:44 -0300

    Add Twitter and X redirects in api.php; update author information in composer.json

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,8 @@ use Stancl\Tenancy\Middleware\InitializeTenancyByPath;
 
 foreach (config('tenancy.central_domains') as $domain) {
     Route::domain($domain)->middleware('api')->group(function (): void {
+        Route::redirect('/twitter', 'https://x.com/bhcosta90');
+        Route::redirect('/x', 'https://x.com/bhcosta90');
         Route::redirect('/linkedin', 'https://linkedin.com/in/bhcosta90');
         Route::redirect('/github', 'https://github.com.br/bhcosta90');
         Route::redirect('/facebook', 'https://www.facebook.com/bhcosta1990');
