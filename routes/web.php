e62d2e4f2824aa065231924071c98e763e7936ca
Bruno Costa <bhcosta90@gmail.com>
2025-06-24 00:24:46 -0300

    Fix return type declaration for root route in web.php

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 foreach (config('tenancy.central_domains') as $domain) {
     Route::domain($domain)->group(function (): void {
-        Route::get('/', fn () => '');
+        Route::get('/', fn (): string => '');
         Route::view('/dashboard', 'dashboard')->name('dashboard');
     });
 }
