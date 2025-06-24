4ba17954d3949f83f76bf621d222d90d9918d701
Bruno Costa <bhcosta90@gmail.com>
2025-06-24 00:07:51 -0300

    Add API middleware to domain routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Middleware\InitializeTenancyByPath;
 
 foreach (config('tenancy.central_domains') as $domain) {
-    Route::domain($domain)->group(function (): void {
+    Route::domain($domain)->middleware('api')->group(function (): void {
         Route::redirect('/linkedin', 'https://linkedin.com/in/bhcosta90');
         Route::redirect('/github', 'https://github.com.br/bhcosta90');
         Route::redirect('/facebook', 'https://www.facebook.com/bhcosta1990');
