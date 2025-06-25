c83444207748e8491a605a4fb6f59c9d3d2c8934
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-06-25 18:25:07 -0300

    Update Envoy tasks to clear cache files and optimize configuration caching

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -18,10 +18,16 @@
 
 @task('install-dependencies')
     cd /var/www/bhcosta90.dev.br/
+    rm bootstrap/cache/{config.php,events.php,packages.php,routes-v7.php,services.php}
     composer install --no-ansi --no-dev --no-interaction --no-plugins --no-progress --no-scripts --optimize-autoloader
+    npm install
+    npm run build
 @endtask
 
 @task('php-artisan-config-cache')
     cd /var/www/bhcosta90.dev.br/
-    php artisan optimize
+    php artisan config:cache
+    php artisan route:cache
+    php artisan event:cache
+    php artisan view:cache
 @endtask
