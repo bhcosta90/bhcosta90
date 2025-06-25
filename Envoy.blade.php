0b8527ab2b9388b985671160396c9a38905f0712
Bruno Henrique da Costa <bhcosta90@gmail.com>
2025-06-25 13:48:07 -0300

    Add Laravel Envoy for deployment automation and update composer dependencies

---
+++ Envoy.blade.php
@@ -0,0 +1,28 @@
+@servers(['web' => ['root@143.110.156.127']])
+
+@story('deploy')
+    update-code
+    install-dependencies
+    php artisan config:cache
+    php artisan route:cache
+    php artisan event:cache
+    php artisan view:cache
+@endstory
+
+@task('update-code')
+    cd /var/www/bhcosta90.dev.br/
+    git pull origin main
+@endtask
+
+@task('install-dependencies')
+    cd /var/www/bhcosta90.dev.br/
+    composer install --no-ansi --no-dev --no-interaction --no-plugins --no-progress --no-scripts --optimize-autoloader
+@endtask
+
+@task('php-artisan-config-cache')
+    cd /var/www/bhcosta90.dev.br/
+    php artisan config:cache
+    php artisan route:cache
+    php artisan event:cache
+    php artisan view:cache
+@endtask
