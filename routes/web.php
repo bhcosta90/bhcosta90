045816cf4f6999b7c5e33a5de5185ebbadeb85ce
Bruno Costa <bhcosta90@gmail.com>
2025-06-27 22:21:19 -0300

    Refactor central domains configuration and add route names for clarity

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 
 foreach (config('tenancy.central_domains') as $domain) {
     Route::domain($domain)->group(function (): void {
-        Route::get('/', fn (): string => '');
-        Route::view('/dashboard', 'dashboard');
+        Route::get('/', fn (): string => '')->name('welcome');
+        Route::view('/dashboard', 'dashboard')->name('dashboard');
     });
 }
 
